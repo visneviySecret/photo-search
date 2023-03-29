1604a31851acc824288de82b257dafd7569fe8d5
visneviySecret <visneviy.secret@yandex.ru>
2023-03-29 19:51:52 +0300

    Fix env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,3 +1,2 @@
-export const API_URL = process.env.NEXT_PUBLIC_API_URL || process.env.API_URL
-export const CLIENT_ID =
-  process.env.NEXT_PUBLIC_CLIENT_ID || process.env.CLIENT_ID
+export const API_URL = process.env.NEXT_PUBLIC_API_URL
+export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
