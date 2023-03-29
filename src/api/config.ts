8bac69f561d6134781f3cc918f3db3a343972189
visneviySecret <visneviy.secret@yandex.ru>
2023-03-29 19:56:32 +0300

    Fix env

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,2 +1,3 @@
 export const API_URL = 'https://api.unsplash.com'
-export const CLIENT_ID = process.env.NEXT_PUBLIC_CLIENT_ID
+export const CLIENT_ID =
+  process.env.NEXT_PUBLIC_CLIENT_ID || process.env.CLIENT_ID
